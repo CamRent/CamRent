dde53f35aeac40a553ab40d8f5bb673496925fc6
Andreas Kollmann <andreas.kollmann@htl.rennweg.at>
2019-05-02 13:43:58 +0200

    Login/Logout button visual switch funktioniert Test mit tatsächlichem Login benötigt

--- Frontend/app/components/logout.js
+++ Frontend/app/components/logout.js
@@ -6,20 +6,56 @@ app.component("logout", {
 app.controller("LogoutController", function ($log, $http, $window) {
     $log.debug("LogoutController()");
 
-    this.submit = () => {
 
-        let url = "../../Backend/logoutUser.php";
-
-        $http({
-            method: 'POST',
-            url: url
-        });
-
-        $window.location.href = "login.html";
+    this.$onInit = () => {
+        this.getStatus();
     };
 
 
-    this.status = () => {
-        return false;
-    }
+    this.getStatus = () => {
+        $http
+            .get('../../Backend/isLoggedIn.php')
+            .then(response => {
+                $log.debug("Response = ", response);
+
+                this.data = response.data;
+                this.status =  this.data.isLoggedIn;
+                console.log(this.status  + "status set");
+            })
+            .catch(response => {
+                $log.error("Da ist etwas nicht so gut gelaufen: " + response);
+            });
+    };
+
+
+
+
+
+
+    this.submit = () => {
+
+        this.getStatus();
+        if (this.status)  {
+            let url = "../../Backend/logoutUser.php";
+
+            $http({
+                method: 'POST',
+                url: url
+            });
+
+            $window.location.href = "index.html";
+
+
+        }
+        else {
+            $window.location.href = "login.html";
+        }
+
+
+
+
+    };
+
+
+
 });
