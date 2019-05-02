7606464f04b72cf89d17daa2eeed48afddf10810
KevChela <kevin.chelappurath@htl.rennweg.at>
2019-05-02 16:16:56 +0200

    Niklas ist scheiße

--- Frontend/app/components/listGegenstand.js
+++ Frontend/app/components/listGegenstand.js
@@ -1,23 +1,21 @@
-    app.component("listGegenstand", {
+app.component("listGegenstand", {
     templateUrl: "components/listGegenstand.html",
     controller: "listGegenstandController"
 });
 
 app.controller("listGegenstandController", function ($http) {
-    this.submit = () => {
 
-        let url = "../../Backend/overviewOfAllItems.php";
-        this.items = {};
+    let url = "../../Backend/overviewOfAllItems.php";
+    this.items = {};
 
-        $http({
-            method: 'POST',
-            url: url
-        }).then(
-            (response) => {
-                console.log(response.data);
-                this.items = response.data;
-            }, function (error) {
-                console.log(error);
-            });
-    };
+    $http({
+        method: 'POST',
+        url: url
+    }).then(
+        (response) => {
+            console.log(response.data);
+            this.items = response.data;
+        }, function (error) {
+            console.log(error);
+        });
 });
